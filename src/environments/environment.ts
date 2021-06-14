d123b314b0336db6821da651d2b98e00c76886f6
Cini19 <39211684+Cini19@users.noreply.github.com>
2021-06-14 12:06:54 +0530

    Add files via upload

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyDp0TThFCkNXpOpTF6MpshFVX5wmyhACL0",
+    authDomain: "voting-d3034.firebaseapp.com",
+    databaseURL: "https://voting-d3034.firebaseio.com",
+    projectId: "voting-d3034",
+    storageBucket: "voting-d3034.appspot.com",
+    messagingSenderId: "821773033518",
+    appId: "1:821773033518:web:d8c3f1866ef9944b"
+  }
+};
+
+
